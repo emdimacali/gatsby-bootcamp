2283dcd04737dc82802b5dea908fc55e2dd2ffce
emdimacali <ejandramae.16@gmail.com>
2019-08-15 22:03:03 +0800

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
             <Head title="Home"/>
             <h1>Hello.</h1>
             <h2>I'm Ejandra, an aspiring game and web developer.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     );
 }
